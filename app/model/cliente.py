257a41abc7152a1e64f85b4ad069b45550569361
hrasam <hrasamgomes@gmail.com>
2026-06-08 16:34:17 -0300

    feat: criacao do model cliente

---
+++ app/model/cliente.py
@@ -0,0 +1,65 @@
+from sqlalchemy import String
+
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
+
+from app.database.base import Base
+
+
+class Cliente(Base):
+
+    __tablename__ = "clientes"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        index=True
+    )
+
+    nome: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    cpf: Mapped[str] = mapped_column(
+        String(14),
+        unique=True,
+        nullable=False
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+
+    telefone: Mapped[str] = mapped_column(
+        String(20),
+        nullable=False
+    )
+
+    cep: Mapped[str] = mapped_column(
+        String(9),
+        nullable=False
+    )
+
+    logradouro: Mapped[str] = mapped_column(
+        String(150),
+        nullable=False
+    )
+
+    bairro: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    cidade: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    estado: Mapped[str] = mapped_column(
+        String(2),
+        nullable=False
+    )
